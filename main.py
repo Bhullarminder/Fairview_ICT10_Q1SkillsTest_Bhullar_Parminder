46ac69c703e633b7be452b6f2f3032d4e793cdc8
Bhullarminder <bhullar.ps@obmontessori.edu.ph>
2026-09-18 13:17:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from pyscript import display, document
 def calculate_total(e):
 
     document.getElementById("receipt").innerHTML = ""
-    americano = document.getElementById('americano').checked
-    spanish_latte = document.getElementById('spanish_latte').checked
-    cold_brew_malt = document.getElementById('cold_brew_malt').checked
-    affogato = document.getElementById('affogato').checked
+
+    americano = document.getElementById("americano").checked
+    spanish_latte = document.getElementById("spanish_latte").checked
+    cold_brew_malt = document.getElementById("cold_brew_malt").checked
+    affogato = document.getElementById("affogato").checked
 
     subtotal = (americano * 120) + (spanish_latte * 150) + (cold_brew_malt * 168) + (affogato * 180)
 
@@ -15,7 +16,7 @@ def calculate_total(e):
 
     total = subtotal + tax
 
-     display("Receipt", target="receipt")
+    display("Receipt", target="receipt")
     display(f"Subtotal: ₱{subtotal}", target="receipt")
     display(f"Tax: ₱{tax}", target="receipt")
     display(f"Total: ₱{total}", target="receipt")
